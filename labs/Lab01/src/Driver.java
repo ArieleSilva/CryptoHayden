f4ccfc39e5576d39e7111cdf7989de4c26000ec8
kevtr0n <kevphayden@gmail.com>
2018-01-18 01:33:44 -0500

    Lab 01 - Style change

--- labs/Lab01/src/Driver.java
+++ labs/Lab01/src/Driver.java
@@ -1,14 +1,15 @@
 /**
- *  File:       Driver.java
- *  Author:     Kevin Hayden
- *  Course:     MSCS 630
- *  Assignment: Lab 01
- *  Due Date:   Wednesday, January 24th, 2018
- *  Version:    1.0
- *
- *  This file contains the Driver class that runs
- *  the enclosed encryption scheme.
+ * File:       Driver.java
+ * Author:     Kevin Hayden
+ * Course:     MSCS 630
+ * Assignment: Lab 01
+ * Due Date:   Wednesday, January 24th, 2018
+ * Version:    1.0
+ * <p>
+ * This file contains the Driver class that runs
+ * the enclosed encryption scheme.
  */
+
 import java.util.Scanner;
 
 /**
@@ -17,56 +18,65 @@ import java.util.Scanner;
  */
 public class Driver {
 
-    /**
-     * The main method
-     * @param args
-     */
-    public static void main(String[] args) {
+  /**
+   * The main method
+   *
+   * @param args
+   */
+  public static void main(String[] args) {
 
-        // Scanner object to read input
-        Scanner input = new Scanner(System.in);
+    // Scanner object to read input
+    Scanner input = new Scanner(System.in);
 
-        // Reads each line and prints each char as an int.
-        while (input.hasNext()) {
+    // Reads each line and prints each char as an int.
+    while (input.hasNext()) {
 
-            // Reads input.
-            String plainText = input.nextLine();
+      // Reads input.
+      String plainText = input.nextLine();
 
-            // Sets plainText to all uppercase.
-            plainText = plainText.toUpperCase();
+      // Sets plainText to all uppercase.
+      plainText = plainText.toUpperCase();
 
-            // Converts string to an array of ints.
-            int[] output = str2int(plainText);
+      // Converts string to an array of ints.
+      int[] output = str2int(plainText);
 
-            // Prints each int in the array.
-            for (int num : output)
-                System.out.print(num + " ");
-            System.out.println();
-        }
+      // Prints each int in the array.
+      for (int num : output)
+        System.out.print(num + " ");
+      System.out.println();
+    }
+  }
+
+  /**
+   * This method takes a String object and converts
+   * each letter to an integer and then adds it to
+   * an integer array. The function then returns the
+   * array.
+   *
+   * @param plainText The string object to be encrypted.
+   * @return an array of ints to be printed
+   */
+  private static int[] str2int(String plainText) {
+
+    // Constant derived from where 'A' starts on the ASCII chart.
+    final int INDEX = 65;
+
+    // int array initialized to the size of the method parameter input.
+    int[] output = new int[plainText.length()];
+
+    // Normalize plainText to all uppercase.
+    plainText = plainText.toUpperCase();
+
+    // Sets char to its corresponding int and adds it to the array
+    for (int i = 0; i < plainText.length(); i++) {
+      char c = plainText.charAt(i);
+      if (c == 32)
+        output[i] = 26;
+      else
+        output[i] = ((int) c) - INDEX;
     }
 
-    /**
-     * This method takes a String object and converts
-     * each letter to an integer and then adds it to
-     * an integer array. The function then returns the
-     * array.
-     *
-     * @param plainText The string object to be encrypted.
-     * @return an array of ints to be printed
-     */
-    private static int[] str2int(String plainText) {
-
-        int[] output = new int[plainText.length()];
-        plainText = plainText.toUpperCase();
-
-        for (int i = 0; i < plainText.length(); i++) {
-            char c = plainText.charAt(i);
-            if (c == 32)
-                output[i] = 26;
-            else
-                output[i] = ((int) c) - 65;
-        }
-        return output;
-    }
+    return output;
+  }
 }
 
